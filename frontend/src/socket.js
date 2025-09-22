64f2464d1465a159f22f74c1f1899bfdd80de3f4
azmal21 <mahammadazmal21@gmail.com>
2025-09-22 15:39:37 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,7 @@
 // socket.js
 import { io } from "socket.io-client";
 
-const SERVER = "https://tic-tac-toe-eulu.onrender.com";
+const SERVER = "import.meta.env.VITE_BACKEND_URL || "https://tic-tac-toe-eulu.onrender.com";
 export const socket = io(SERVER, { autoConnect: true });
+
 
